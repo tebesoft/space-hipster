aebd1dbe8e08fb5055de4033a621fd9c7b49476d
tebesoft <tebesoft@gmail.com>
2018-12-28 16:12:55 +0300

    bullet prefab

---
+++ app/scripts/objects/bullet.js
@@ -0,0 +1,28 @@
+export default class Bullet extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene, x, y) {
+    super(scene, x, y, 'bullet');
+    this.bulletSpeed = -1000;
+    this.scene.add.existing(this);
+  }
+
+  setupWorldCollision() {
+    this.body.setCollideWorldBounds(true);
+
+    this.body.onWorldBounds = true;
+
+    this.body.world.on('worldbounds', function(body) {
+      // Check if the body's game object is the sprite you are listening for
+      if (body.gameObject === this) {
+        // Stop physics and render updates for this object (Kill)
+        this.setVisible(false);
+        this.setActive(false);
+      }
+    }, this);
+  }
+
+  activate() {
+    this.setActive(true);
+    this.setVisible(true);
+    this.body.setVelocityY(this.bulletSpeed);
+  }
+}
